9d7bf91ea0f9268d49e9e417cdef45338a72506f
okarslan <okarslan@dabnet.com.tr>
2024-06-01 23:52:43 +0300

    food app cretaed rn

---
+++ components/CategoryGrid.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function CategoryGrid({title,color}) {
+  return (
+    <View>
+      <Text>{title}</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
---
+++ screens/CategoriesScreen.js
@@ -0,0 +1,21 @@
+import { FlatList, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { CATEGORIES } from '../data/dummy-data'
+import CategoryGrid from '../components/CategoryGrid'
+  
+export default function CategoriesScreen() {
+    function renderCategoryItem(itemData){
+        return (
+            <CategoryGrid title={itemData.item.title} color={itemData.item.color} />
+        )
+    }
+  return (
+    <FlatList
+    data={CATEGORIES}
+    keyExtractor={(item) => item.id}
+    renderItem={renderCategoryItem}
+     />
+  )
+}
+
+const styles = StyleSheet.create({})
