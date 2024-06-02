1a02fe31d7dbd2aaa3deb13bc81448cb156ff075
okarslan <okarslan@dabnet.com.tr>
2024-06-02 11:56:34 +0300

    food list screen design

--- components/FoodOverViewScreen.js
+++ components/FoodOverViewScreen.js
@@ -1,13 +1,36 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { FlatList, StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { FOODS } from "../data/dummy-data";
+import FoodItem from "./FoodItem";
 
-export default function FoodOverViewScreen({route}) {
-    const categoryId = route.params.categoryId
-  return (
-    <View>
-      <Text>{categoryId}</Text>
-    </View>
-  )
-}
-
-const styles = StyleSheet.create({})
+export default function FoodOverViewScreen({ route }) {
+    const categoryId = route.params.categoryId;
+    const displayedFoods = FOODS.filter((foodItem) => {
+      return foodItem.categoryIds.indexOf(categoryId) >= 0;
+    });
+  
+    function renderFoodItem(itemData) {
+     
+      const foodItemProps = {
+        id: itemData.item.id,
+        title: itemData.item.title,
+        imageUrl: itemData.item.imageUrl,
+        affordability: itemData.item.affordability,
+        complexity: itemData.item.complexity,
+      };
+  
+      return <FoodItem {...foodItemProps} />;
+    }
+  
+    return (
+      <View>
+        <FlatList
+          data={displayedFoods}
+          keyExtractor={(item) => item.id}
+          renderItem={renderFoodItem}
+        />
+      </View>
+    );
+  }
+  
+  const styles = StyleSheet.create({});
